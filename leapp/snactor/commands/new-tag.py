c5a77bd2f7ce496bdf84c1a7724f311899a6ad30
Vinzenz Feenstra <vfeenstr@redhat.com>
2018-04-04 09:41:43 +0200

    Fix new tag

--- leapp/snactor/commands/new-tag.py
+++ leapp/snactor/commands/new-tag.py
@@ -9,7 +9,7 @@ from leapp.snactor.utils import find_project_basedir, make_name, make_class_name
 @command('new-tag', help='Create a new tag')
 @command_arg('tag-name')
 @requires_project
-def cli(tag_name):
+def cli(args):
     basedir = find_project_basedir('.')
     if not basedir:
         raise UsageError('This command must be executed from the project directory')
@@ -18,7 +18,7 @@ def cli(tag_name):
     if not os.path.isdir(basedir):
         os.mkdir(basedir)
 
-    tag_path = os.path.join(basedir, tag_name.lower() + '.py')
+    tag_path = os.path.join(basedir, args.tag_name.lower() + '.py')
     if os.path.exists(tag_path):
         raise UsageError("File already exists: {}".format(tag_path))
 
@@ -28,7 +28,7 @@ def cli(tag_name):
 
 class {tag_name}Tag(Tag):
     name = '{tag}'
-'''.format(tag_name=make_class_name(tag_name), tag=make_name(tag_name)))
+'''.format(tag_name=make_class_name(args.tag_name), tag=make_name(args.tag_name)))
 
-    sys.stdout.write("New tag {} has been created in {}\n".format(make_class_name(tag_name),
+    sys.stdout.write("New tag {} has been created in {}\n".format(make_class_name(args.tag_name),
                                                                   os.path.realpath(tag_path)))
